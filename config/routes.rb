e698f33dce8519a71d12adc6cac15813bf274e6d
joebumbulis <joebumbulis@gmail.com>
2019-05-15 11:12:29 -0500

    adds about page and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
-  get 'user/index'
+  get 'static_pages/about'
+
   resources :projects, param: :slug do
     resources :proposals, param: :slug do
       member do
