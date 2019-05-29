83be71880145cefdc5d797a21b6b4c9ab473ef48
joebumbulis <joebumbulis@gmail.com>
2019-05-29 12:22:44 -0500

    cleans up routes and header stylesheet

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
 
   resources :users
 
-  # get 'new_user_path', to: 'users#new', as: "signup_path"
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
